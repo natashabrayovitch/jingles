22ecd447bcc11c5ca963b76b1ca9e4baa459f738
Natasha Brayovitch <stanticdoo@gmail.com>
2019-03-09 21:56:13 +0100

    #3 Moveing the covers data model to a proper front-end data source

--- src/Technologies.js
+++ src/Technologies.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
 import Stack from './Stack';
-import jinglesLogo from './JinglesVB.png';
+import getTechnologies from './technologies-get';
 
 export default function Technologies() {
     return (
-      <div>
-        <div className='container'>
-          <Stack id='jingles' name='Jingles' logo={jinglesLogo} />
+        <div>
+            <div className='container'>
+                {
+                    getTechnologies().map(stack => (
+                        <Stack 
+                        key={stack.id}
+                        id={stack.id}
+                        name= {stack.name}
+                        logo= {stack.logo} />
+                    ))
+                }
+            </div>
         </div>
-      </div>
     );
-  }
+}
---
+++ src/technologies-get.js
@@ -0,0 +1,8 @@
+import jinglesLogo from './JinglesVB.png';
+
+
+export default function getTechnologies() {
+    return [
+        { id: 'jingles', name: 'Jingles', logo: jinglesLogo }
+    ];
+}
