18ed7f202fc0d2a7c2fa43d7e7e62cd567153664
Natasha Brayovitch <stanticdoo@gmail.com>
2019-03-11 13:57:44 +0100

    imported collection photos

--- src/technologies-get.js
+++ src/technologies-get.js
@@ -1,9 +1,13 @@
-import jinglesLogo from './JinglesVB.png';
 
+import ssCollage from './ss-collage.png';
+import awCollage from './aw-collage.png';
+import denimCollage from './denim-collage.png';
 
 
 export default function getTechnologies() {
     return [
-        { id: 'jingles', name: 'Jingles', logo: jinglesLogo },
+        { id: 'ss18', name: 'SS18', logo: ssCollage },
+        { id: 'aw18', name: 'AW18', logo: awCollage },
+        { id: 'denim18', name: 'Denim18', logo: denimCollage },
     ];
 }
