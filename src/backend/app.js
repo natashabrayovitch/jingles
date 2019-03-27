96e0a260b560684ac85f21a4a0ae524070e73d31
Natasha Brayovitch <stanticdoo@gmail.com>
2019-03-27 18:27:23 +0000

    #19 Return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const getShows = require('./shows');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/backend/shows.js
@@ -0,0 +1,16 @@
+module.exports = function getShows() {
+    return [{
+        id: 'ss18',
+        name: 'SS18',
+        synopsis: 'Summer Solstice 2018 collection was all about the heat.Fantastically we were blessed with the most incredible summer. This collection has absolutely all the celebration of it and is down way pride to it. The wonderful film is created by Ed Mathews .        Queeny is the model with fantastic crop celebrates the return of the afro.        Short blond hair is a touch of the “Mia Farrow” to it . The interpretation of Parisian girl with the gentle softly waved bob, the freckles as the result of a long hot summer bringing out the glow and the beauty in the face and the skin. Simple blond layered haircut with the touch of pink or touch of salmon in the quality of the hair which is so beautiful.The finish off with the showstop of the return of the perms: bright red hair with softly permed curls (front cover in many countries world wide).'
+    }, {
+        id: 'aw18',
+        name: 'AW18',
+        details: 'The Winter Solstice 2018. The theme of this collection was wide cross section of the work that we typically do in the salon which is beautiful quality well finished hair, touching different skills that we use in the salon. Skills of the haircuts, the skill of the finishing, the cleanness of the colour - all of these are evident in this Autumn/Winter ‘18 collection . “The inspiration I’ve gone through : the colours are very very clean, very crisp, beautifully presented . It’s expensive quality looking hair.”'
+    }, {
+        id: 'denim18',
+        name: 'Denim18',
+        details: 'Denim collection 2018. The summer special collection took 3 of the girls who featured in Winter Solstice collection . We started with the “Double Denim“ summer celebration where we reinvented three different looks. The main inspiration came from the summer fresh feeling set against the very best of the summer expression pictures that David Hockney made while he was living in California.'
+    }
+    ]
+}
