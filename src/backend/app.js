e9817bd7dc2dd43530dc59708c9a84c28b903ad0
Natasha Brayovitch <stanticdoo@gmail.com>
2019-04-02 00:05:02 +0100

    23 tricky

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,10 +7,10 @@ const getShows = require('./shows');
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 // Handle React routing, return all requests to React app
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/backend/build/precache-manifest.270f1f58f9878b5ce443c1f1ebfc4115.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "96a8391789b727f5bbcf",
+    "url": "/static/css/main.3b55957b.chunk.css"
+  },
+  {
+    "revision": "96a8391789b727f5bbcf",
+    "url": "/static/js/main.a7cb140a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "06f4c5817af8527ae2b7",
+    "url": "/static/js/2.f7b08965.chunk.js"
+  },
+  {
+    "revision": "ef0557ee6c078188a33ade8a55dcbb84",
+    "url": "/static/media/ss-collage.ef0557ee.png"
+  },
+  {
+    "revision": "81ae0ef1b2938c58d69daef3dab753d5",
+    "url": "/static/media/aw-collage.81ae0ef1.png"
+  },
+  {
+    "revision": "11b40afc474a1e93eddeabebdd69bed3",
+    "url": "/static/media/denim-collage.11b40afc.png"
+  },
+  {
+    "revision": "20ae2d033a5086383be08182d3a66f2a",
+    "url": "/index.html"
+  }
+];
