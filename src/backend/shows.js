3178d411dfaad0b129d47bb7b59975a7c90c149c
Natasha Brayovitch <stanticdoo@gmail.com>
2019-04-16 13:47:20 +0100

    working on #27

--- src/backend/shows.js
+++ src/backend/shows.js
@@ -6,10 +6,10 @@ module.exports = function getShows() {
     }, {
         id: 'aw18',
         name: 'AW18',
-        details: 'The Winter Solstice 2018. The theme of this collection was wide cross section of the work that we typically do in the salon which is beautiful quality well finished hair, touching different skills that we use in the salon. Skills of the haircuts, the skill of the finishing, the cleanness of the colour - all of these are evident in this Autumn/Winter ‘18 collection . “The inspiration I’ve gone through : the colours are very very clean, very crisp, beautifully presented . It’s expensive quality looking hair.”'
+        synopsis: 'The Winter Solstice 2018. The theme of this collection was wide cross section of the work that we typically do in the salon which is beautiful quality well finished hair, touching different skills that we use in the salon. Skills of the haircuts, the skill of the finishing, the cleanness of the colour - all of these are evident in this Autumn/Winter ‘18 collection . “The inspiration I’ve gone through : the colours are very very clean, very crisp, beautifully presented . It’s expensive quality looking hair.”'
     }, {
         id: 'denim18',
         name: 'Denim18',
-        details: 'Denim collection 2018. The summer special collection took 3 of the girls who featured in Winter Solstice collection . We started with the “Double Denim“ summer celebration where we reinvented three different looks. The main inspiration came from the summer fresh feeling set against the very best of the summer expression pictures that David Hockney made while he was living in California.'
+        synopsis: 'Denim collection 2018. The summer special collection took 3 of the girls who featured in Winter Solstice collection . We started with the “Double Denim“ summer celebration where we reinvented three different looks. The main inspiration came from the summer fresh feeling set against the very best of the summer expression pictures that David Hockney made while he was living in California.'
     }];
 }
