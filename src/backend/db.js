37e0e0b7ae2c3214b835f641b2b43c101f1f02dd
Natasha Brayovitch <stanticdoo@gmail.com>
2019-04-19 20:45:22 +0200

    #30broken in prod -point to DEV/PRD db dynamically

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,23 +1,38 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 const assert = require('assert');
-const db = {
-    host: 'localhost',
-    port: '27017',
-    name: 'useJingles',
-    user: 'useJingles',
-    pwd: 'kodflix',
-};
-const url = `mongodb://${db.user}:${db.pwd}@${db.host}:${db.port}/${db.name}`;
+require('dotenv/config');
 
-module.exports = { connect };
+let url = "";
+let dbName = "";
+if (process.env.NODE_ENV) {
+    console.log(process.env.NODE_ENV);
+    //This is PRODUCTION ENVIROMENT
+    url = process.env.DB_URL_PRD;
+    console.log(url);
+    dbName = "UseJingles"; //capital letter in prod
+} else {
+    //THIS IS DEV ENVIROMENT
+    console.log('we are in dev local');
+    url = "mongodb://localhost:27017";
+    dbName = "useJingles"; //lower case in development
+}
+
+//const url = process.env.NODE_ENV === 'production' ?
+  //  process.env.DB_URL_PRD :
+    //process.env.DB_URL_DEV;
+
 
 function connect() {
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(url, function (err, client) {
+    const client = new MongoClient(url, {useNewUrlParser: true});
+    return new Promise(resolve => {
+        client.connect(function(err) {
             assert.equal(null, err);
+            const db = client.db(dbName);
             console.log('Connected successfully to server');
-            const dbo = client.db(db.name);
-            resolve(dbo);
+            const dbo = client.db(dbName);
+            resolve(db);
         });
     });
 }
+module.exports = { connect };
